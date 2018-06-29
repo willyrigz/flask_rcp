a225fcf47cf011ca508964c051169ef45457ebdc
willyrigz <willyrigz@gmail.com>
2018-06-29 08:37:19 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+# DATABASE_URL: postgres://qcqgafnbwwtqdp:95c7a83b4043b97bdbc3c1c671906ad33c4916af4d96ae9db52df732562ec811@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d3gl3fh4221usg
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	url = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin,IE&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+	
+	r = requests.get(url).json()
+	
+	weather = {
+		'city' : r['name'],
+		'humidity' : r['main']['humidity'],
+		'pressure' : r['main']['pressure'],
+		'temperature' : r['main']['temp'],
+		'description' : r['weather'][0]['description'],
+		'icon' : r['weather'][0]['icon'],
+
+	}
+
+	print(weather)
+
+	return render_template("index.html", weather=weather)
+
+@app.route("/records")
+def records():
+	return render_template("records.html")
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
